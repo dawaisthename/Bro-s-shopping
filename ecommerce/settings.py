646be0f6212089089e55272a1d9fe34b00ff4b29
Dawde Seifu <seifudawde@gmail.com>
2022-09-14 15:18:43 +0300

    update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,3 +133,4 @@ LOGIN_URL= 'login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK="bootstrap4"
+django_heroku.settings(locals())
